26480256cf0055a3df131b85b3f3ddffb50e35ad
James Watt <wattjms1993@gmail.com>
2023-12-13 11:54:13 +0530

    props

---
+++ 11-12-2023/src/About.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+      james
+    </div>
+  );
+}
+
+export default About;
---
+++ 11-12-2023/src/Contact.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Contact() {
+  return (
+    <div className="App">
+      james
+    </div>
+  );
+}
+
+export default Contact;
