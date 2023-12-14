e099b962ff9faf9549101471b47e433c172f2cb8
James Watt <wattjms1993@gmail.com>
2023-12-14 17:13:37 +0530

    ka-ecommerce

---
+++ 14-12-2023/destruct/src/Product/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.css';
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+export default function Product(props) {
+    const { id, imageUrl, name, description, price } = props
+
+    return (
+        <div
+            key={id}
+            className={"product"}
+        >
+            <img className={"image-product"} src={imageUrl} alt={name} />
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <span>${price}</span>
+            <div>
+                <button
+                    className="snipcart-add-item"
+                    data-item-id={id}
+                    data-item-image={imageUrl}
+                    data-item-name={name}
+                    data-item-url="/"
+                    data-item-price={price}
+                >
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    );
+}
