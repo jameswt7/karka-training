1c2064e620a23fc5caa5ed5c790eae96aa885ed3
jameswt7 <152612309+jameswt7@users.noreply.github.com>
2023-12-26 12:35:08 +0530

    Update Login.js

--- 22-12-2023/reactpro/src/components/Login.js
+++ 22-12-2023/reactpro/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react"
+import React, { useState,useEffect } from "react";
 
+export const Login = () => {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+  });
 
-const Login = () => {
-    
- 
-    return (
-        <h1>Hello</h1>
-    )
-}
-export default Login;
+  const handleClick = () => {
+    console.log(user);
 
+    if (localStorage.getItem("userData")) {
+      const local = JSON.parse(localStorage.getItem("userData")) || [];
+      const foundUser = local.find(
+        (item) =>
+          item.userName === user.userName && item.password === user.password
+      );
+
+      if (foundUser) {
+        alert("Login success");
+        window.location.href = "/Home";
+      } else {
+        alert("Login failed");
+      }
+    }
+  };
+  useEffect(()=>{
+    setUser({userName:"",password:""})
+  }, []) 
+
+  return (
+    <div>
+      <label>userName</label>
+      <input
+        type={"text"}
+        value={user.userName}
+        onChange={(e) => setUser({ ...user, userName: e.target.value })}
+      />
+      <label>password</label>
+      <input
+        type={"password"}
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={handleClick}>submit</button>
+      <a href="/Register">create account</a>
+    </div>
+  );
+};
