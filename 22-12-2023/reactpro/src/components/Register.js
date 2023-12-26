f1918269bdde898211914edfc9c84fc663decdb1
jameswt7 <152612309+jameswt7@users.noreply.github.com>
2023-12-26 12:37:12 +0530

    Update Register.js

--- 22-12-2023/reactpro/src/components/Register.js
+++ 22-12-2023/reactpro/src/components/Register.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from 'react'
 
-const Register = () => {
-    return <h1>Register Page</h1>;
-  };
-  
-  export default Register;
+export const Register = () => {
+    const [register, setRegister] = useState({
+        userName: "",
+        password: "",
+    })
+    const handleClick = () => {
+        console.log(register)
+        if(register.userName&&register.password){
+                const local=JSON.parse(localStorage.getItem("userData"))||[];
+                local.push(register);
+                localStorage.setItem("userData",JSON.stringify(local))
+                setRegister({
+                    userName: "",
+                    password: "",
+                })
+        }else{
+            alert("please enter username and password")
+        }
+    }
+  return (
+    <div>
+        <label>UserName</label>
+        <input type="text" value={register.userName} onChange={(e) => setRegister({...register, userName: e.target.value})}/>
+        <label>Password</label>
+        <input type="password" value={register.password} onChange={(e) => setRegister({...register, password: e.target.value})}/>
+        <button onClick={handleClick}>Register</button>
+    </div>
+  )
+}
