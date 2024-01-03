1559208f390d21e128d314b498dcadaa81322f97
James Watt <wattjms1993@gmail.com>
2024-01-03 17:27:42 +0530

    Html Task

---
+++ Html and Css Task/task3 page/one.js
@@ -0,0 +1,20 @@
+var a=10;
+
+if ( a %2 ===0) 
+{
+    console.log("the no is even")
+} else {
+    console.log("the no is odd")
+}
+
+var b=[1,2,3,4,5,6];
+for (let index = 0; index < b.length; index++) {
+    const element = b[index];
+    if ( element %2 ===0) 
+    {
+        console.log("the no is even",element)
+    } else {
+        console.log("the no is odd",element)
+    }
+        // console.log(element)
+}
